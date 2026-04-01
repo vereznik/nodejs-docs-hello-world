8d21e3dafaa33b59b7c8b118b32231016f89b4c4
vereznik <60116411+vereznik@users.noreply.github.com>
2026-04-01 15:34:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // https://github.com/microsoft/Web-Dev-For-Beginners/tree/main/7-bank-project/api
 // ***************************************************************************
 
-
+respons.end("Hello World");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
